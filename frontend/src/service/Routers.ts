25ae10ac7aeb28bb2cf811df41fc2dbe44c4f69b
Karine <karine.costa@somagrupo.com.br>
2023-03-27 18:09:02 -0300

    axios: routers

---
+++ frontend/src/service/Routers.ts
@@ -0,0 +1,30 @@
+import API from "./Api";
+
+export function createAuth(data: { usercode: string; password: string }) {
+  return API.post("auth/login", data);
+}
+
+export function createUser(data: {
+  code: string;
+  name: string;
+  email: string;
+  password: string;
+}) {
+  return API.post("users/register", data);
+}
+
+export function deleteUser(id: string) {
+  return API.delete(`users/${id}`);
+}
+
+export function createCheck(data: { usercode: string }) {
+  return API.post("work/", data);
+}
+
+export function getChecks(id: string) {
+  return API.get(`/work/${id}`);
+}
+
+export function updateCheck(data: { usercode: string; date: string }) {
+  return API.put(`work/`, data);
+}
